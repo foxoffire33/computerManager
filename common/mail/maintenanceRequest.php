693834922d27682a7031590e7f5dc00708f9ea73
reinierdlp <reinier@releaz.nl>
2016-04-20 13:50:06 +0200

    mails

--- common/mail/maintenanceRequest.php
+++ common/mail/maintenanceRequest.php
@@ -1,6 +1,6 @@
 Beste <?= $model->customerName; ?> Uw aanvraag wordt verwerkt, Wij nemen binnen twee werk dagen contact met u op.
 <br/>
-U kunt inloggen met de volgende gegevens<br/>
+U kunt inloggen met de volgende gegevens.<br/>
 <ul>
     <li><?= Yii::t('maintenaceRequest', 'Username, {username}', ['username' => $model->email]) ?></li>
     <li><?= Yii::t('maintenaceRequest', 'Password, {password}', ['password' => $model->userPassword]) ?></li>
--- common/mail/maintenanceRequestExisting.php
+++ common/mail/maintenanceRequestExisting.php
@@ -1,1 +1,1 @@
-Beste Mr/Mv <?= $model->computer->customer->name; ?> Uw aanvraag wordt werkt wij nemen binnen twee werk dagen contact met u op.
+Beste <?= $model->computer->customer->name; ?> Uw aanvraag wordt werkt, wij nemen binnen twee werk dagen contact met u op.
